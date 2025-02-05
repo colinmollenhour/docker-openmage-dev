7c65ba15388226fdc17906948654a156100e9147
Colin Mollenhour <colin@mollenhour.com>
2025-02-05 09:48:39 -0500

    Fix broken php-fpm flavor

--- 8.2/fpm/entrypoint.sh
+++ 8.2/fpm/entrypoint.sh
@@ -12,6 +12,14 @@ if [ "$XDEBUG_CONFIG" ]; then
     done
 fi
 
+# Configure PHP-FPM using environment variables
+if test -f /usr/local/etc/php-fpm.d/www.conf.template && ! test -f /usr/local/etc/php-fpm.d/www.conf; then
+    export PM_MAX_CHILDREN=${PM_MAX_CHILDREN:-5}
+    export PM_START_SERVERS=${PM_START_SERVERS:-2}
+    export PM_MIN_SPARE_SERVERS=${PM_MIN_SPARE_SERVERS:-1}
+    export PM_MAX_SPARE_SERVERS=${PM_MAX_SPARE_SERVERS:-3}
+    while read -r line; do eval echo \"$line\"; done < /usr/local/etc/php-fpm.d/www.conf.template > /usr/local/etc/php-fpm.d/www.conf
+fi
 
 # Execute the supplied command
 exec "$@"
